697ed69c2832fe3cc2812f75da2238976d17dbf1
Rustam Saidgareev <rsaidgareev@gmail.com>
2020-06-08 17:48:50 -0400

    changes3

--- script/script.js
+++ script/script.js
@@ -1,23 +1,15 @@
 let money = 1200;
-
-console.log(money);
-
 let income = 'ebay';
-
-console.log(income);
-
 let addExpenses = 'Appartment, Insurance, Loan';
-
-console.log(addExpenses.length);
-console.log(addExpenses.toLowerCase());
-console.log(addExpenses.split(', '));
-
 let deposit = true;
-
-console.log(deposit);
-
 let mission = 25;
 let period = 7;
+
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
+console.log(addExpenses.length);
+console.log(addExpenses.toLowerCase().split(', '));
 
 let budgetDay = (money / 30);
 
