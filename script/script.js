19a4ebf4a21e316d62721e49320a21c52b43f8cc
Rustam Saidgareev <rsaidgareev@gmail.com>
2020-06-08 18:53:35 -0400

    changes5

--- script/script.js
+++ script/script.js
@@ -9,8 +9,9 @@ console.log(typeof(money));
 console.log(typeof(income));
 console.log(typeof(deposit));
 console.log(addExpenses.length);
+console.log('Цель заработать ' + mission + ' долларов');
+console.log('Период равен ' + period + ' месяцев');
 console.log(addExpenses.toLowerCase().split(', '));
 
-let budgetDay = (money / 30);
-
+let budgetDay = money/30;
 console.log(budgetDay);
