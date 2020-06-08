00c8715eb264afcc81b071d76941700f651d7463
Rustam Saidgareev <rsaidgareev@gmail.com>
2020-06-08 13:15:57 -0400

    changes1

--- script/script.js
+++ script/script.js
@@ -1,14 +1,24 @@
 let money = 1200;
+
 console.log(money);
+
 let income = 'ebay';
+
 console.log(income);
+
 let addExpenses = 'Appartment, Insurance, Loan';
+
 console.log(addExpenses.length);
 console.log(addExpenses.toLowerCase());
 console.log(addExpenses.split(', '));
+
 let deposit = true;
+
 console.log(deposit);
+
 let mission = 25;
 let period = 7;
+
 let budgetDay = (1200 / 30);
+
 console.log(budgetDay);
